640568d1ceb7e97298771e2027fb31cb71ca0eb5
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-11 22:56:19 -0300

    lesson 14 finished

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnChanges, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit {
-  public title: string = 'Hello Angular component!'
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = 'Hello Angular component!'
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    console.log('título alterado')
+  }
 }
