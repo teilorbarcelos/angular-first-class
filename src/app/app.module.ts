70d67832607a57754d89b2c4b9324187d34ea391
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-15 17:53:18 -0300

    two-way biding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { DataBidingComponent } from './data-biding/data-biding.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TitleComponent } from './title/title.component'
+import { DataBidingComponent } from './data-biding/data-biding.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TitleComponent,
-    DataBidingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TitleComponent, DataBidingComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
