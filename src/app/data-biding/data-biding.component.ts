aefc5bc610b3d874190e23281ec7927603512d41
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-15 17:40:17 -0300

    event biding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 
+interface CursorPositionProps {
+  x: number
+  y: number
+}
+
 @Component({
   selector: 'app-data-biding',
   templateUrl: './data-biding.component.html',
@@ -9,8 +14,20 @@ export class DataBidingComponent implements OnInit {
   public name: string = 'Teilor'
   public age: number = 34
   public isDisabled: boolean = true
+  public cursorPosition: CursorPositionProps = { x: 0, y: 0 }
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public alertInfo(message: string) {
+    alert(`Mensagem: ${message}`)
+  }
+
+  public eventInfo(event: MouseEvent) {}
+
+  public mouseMoveAction(event: MouseEvent) {
+    this.cursorPosition.x = event.offsetX
+    this.cursorPosition.y = event.offsetY
+  }
 }
