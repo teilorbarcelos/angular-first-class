ea22e165904aa1a1fd6f5dc4d46cb21773ca5ae1
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-21 21:28:48 -0300

    ngFor done

--- src/app/structural-directives/structural-directives.component.ts
+++ src/app/structural-directives/structural-directives.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 
+interface ListItemProps {
+  name: string
+  age: number
+}
+
 @Component({
   selector: 'app-structural-directives',
   templateUrl: './structural-directives.component.html',
@@ -8,6 +13,11 @@ import { Component, OnInit } from '@angular/core'
 export class StructuralDirectivesComponent implements OnInit {
   public condition: boolean = true
   public conditionClick: boolean = true
+  public list: ListItemProps[] = [
+    { name: 'Pedrinho', age: 35 },
+    { name: 'Luizinho', age: 36 },
+    { name: 'Zezinho', age: 37 },
+  ]
 
   constructor() {}
 
@@ -19,5 +29,13 @@ export class StructuralDirectivesComponent implements OnInit {
 
   public switch() {
     this.conditionClick = !this.conditionClick
+  }
+
+  public addToList() {
+    this.list.push({ name: 'Tio Patinhas', age: 78 })
+  }
+
+  public removefromList(item: number) {
+    this.list.splice(item, 1)
   }
 }
