81629ea583c4c1b2a644f56a414eb7d64e8c3ecd
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-24 22:24:32 -0300

    input data, like react props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ import { Component, OnInit } from '@angular/core'
       <h1>Directives attributes lesson</h1>
       <h3>segundo ng content</h3>
     </app-attributes-directives>
+    <app-input [value]="inputValue"></app-input>
+    <br />
+    <button (click)="addToCounter()">Add more</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
+  public inputValue: number = 0
+
   constructor() {}
+
+  public addToCounter() {
+    this.inputValue += 1
+  }
 
   ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FirstSharedComponentComponent } from './first-shared-component/first-shared-component.component'
+import { InputComponent } from './input/input.component'
 
 @NgModule({
-  declarations: [FirstSharedComponentComponent],
+  declarations: [FirstSharedComponentComponent, InputComponent],
   imports: [CommonModule],
-  exports: [FirstSharedComponentComponent],
+  exports: [FirstSharedComponentComponent, InputComponent],
 })
 export class SharedModule {}
