a4d9e91a5f589c2fb5b2444c9ba983f10d9195f9
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-03 22:01:49 -0300

    form submit working :tada:

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
@@ -16,4 +17,8 @@ export class FormsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  public submitForm(form: NgForm) {
+    console.log(form)
+  }
 }
