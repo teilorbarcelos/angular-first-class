77fafca70e61719cce648c831a00375909f90da6
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-03 21:32:54 -0300

    checkbox and select input ngmodel

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
+  public foodList: { label: string; value: string }[] = [
+    { label: 'Pizza', value: 'pizza' },
+    { label: 'Hamburger', value: 'hamburger' },
+    { label: 'Lazanha', value: 'lazanha' },
+    { label: 'Coxinha', value: 'coxinha' },
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
